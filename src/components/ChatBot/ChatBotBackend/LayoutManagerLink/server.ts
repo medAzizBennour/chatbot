960e82310e63428a4af869e55f4e33dfa4be3526
Borhen Benltaief <borhen.benletaief@gmail.com>
2023-04-06 09:38:36 +0100

    Configured the server correctly

--- src/components/ChatBot/ChatBotBackend/LayoutManagerLink/server.ts
+++ src/components/ChatBot/ChatBotBackend/LayoutManagerLink/server.ts
@@ -1,8 +1,20 @@
 import express, { Application } from "express";
+import WebSocket from "ws";
 
 const app: Application = express();
 
 const PORT: string | number = process.env.PORT || 5000;
+
+// create a WebSocket server
+const wss = new WebSocket.Server({ port: 8000 });
+
+wss.on("connection", (ws: WebSocket) => {
+    console.log("WebSocket client connected");
+
+    // send a JSON message to the client
+    const message = { type: "message", data: { text: "Hello, client!" } };
+    ws.send(JSON.stringify(message));
+});
 
 app.listen(PORT, () =>
     console.log(` 📡 Backend server: ` + ` Running  on port ${PORT}`)
