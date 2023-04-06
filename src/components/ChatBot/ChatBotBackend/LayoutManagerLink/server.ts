4ab3d92c680dea21183473bd6888fdcb665a5c3f
Borhen Benltaief <borhen.benletaief@gmail.com>
2023-04-06 09:57:05 +0100

    Navigate works

--- src/components/ChatBot/ChatBotBackend/LayoutManagerLink/server.ts
+++ src/components/ChatBot/ChatBotBackend/LayoutManagerLink/server.ts
@@ -6,13 +6,16 @@ const app: Application = express();
 const PORT: string | number = process.env.PORT || 5000;
 
 // create a WebSocket server
-const wss = new WebSocket.Server({ port: 8000 });
+const wss = new WebSocket.Server({ port: 4000 });
 
 wss.on("connection", (ws: WebSocket) => {
     console.log("WebSocket client connected");
 
     // send a JSON message to the client
-    const message = { type: "message", data: { text: "Hello, client!" } };
+    const message = {
+        type: "message",
+        data: { action: "navigate", parameters: { page: "Dashboard" } },
+    };
     ws.send(JSON.stringify(message));
 });
 
